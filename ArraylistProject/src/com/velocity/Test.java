810a5fd2af46ad89ff676aebc4dd5f2c8429ce23
mahesh8005github <maheshtavare6@gmail.com>
2024-06-28 16:59:44 +0530

    Arraylist code added

---
+++ ArraylistProject/src/com/velocity/Test.java
@@ -0,0 +1,20 @@
+package com.velocity;
+
+
+import java.util.ArrayList;
+
+public class Test {
+public static void main(String[] args) {
+	
+	ArrayList<Integer>al=new ArrayList<Integer>();
+	
+	al.add(10);
+	al.add(20);
+	al.add(30);
+	al.add(40);
+	al.add(50);
+	
+	System.out.println(al);
+	
+}
+}
