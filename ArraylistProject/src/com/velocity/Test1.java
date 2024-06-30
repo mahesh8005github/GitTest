7dade6d5d905050461f48d3ff0dbfae51828181b
mahesh8005github <maheshtavare6@gmail.com>
2024-06-30 11:43:10 +0530

    Test1 file added

---
+++ ArraylistProject/src/com/velocity/Test1.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.HashSet;
+
+public class Test1 {
+public static void main(String[] args) {
+	
+	HashSet<String> hs=new HashSet<String>();
+	
+	hs.add("mahesh");
+	hs.add("prasad");
+	hs.add("ashwini");
+	hs.add("mayuri");
+	hs.add("rahul");
+	
+	System.out.println(hs);
+	
+}
+}
